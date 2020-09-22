5a0a1dbecf42eabba5bfdd010fa3d31f6eea9264
soapyuki <dongzhiliwansui@163.com>
2020-09-22 15:50:43 +0800

    第二次提交修复common_func环境变量url切换问题

--- case/common_func.py
+++ case/common_func.py
@@ -1,11 +1,11 @@
-import re, allure
+import re, allure, os
 
 
 # 公共获取token操作函数
 @allure.step("登录步骤")
 def login(s, username="soapyuki", password="1234abcd"):
     '''登录获取token或获取cookie'''
-    url = "http://127.0.0.1:7777/admin/login/?next=/admin/"
+    url = os.environ["admin_host"]+"/admin/login/?next=/admin/"
     r = s.get(url=url)
     cmt = re.findall("'csrfmiddlewaretoken' value='(.+?)'", r.text)
     body = {
